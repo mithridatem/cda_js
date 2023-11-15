3c02797e255eddbab4713af9c25d934f421af3a3
mithridatem <mathieumithridate@adrar-formation.com>
2023-11-15 16:28:08 +0100

    ajout exemple fonction et attribut static

--- exercice13/main.js
+++ exercice13/main.js
@@ -12,11 +12,14 @@ Ajouter un attribut nbrEtage à la maison,
 Afficher dans la console la surface pour 3 étages en mètre carrés. 
  */
 class Maison {
+    static nbrDeMaison = 0;
 	constructor(newNom, newLongueur, newLargeur, newEtage = 1) {
 		this.nom = newNom;
 		this.longueur = newLongueur;
 		this.largeur = newLargeur;
 		this.nbrEtage = newEtage;
+        Maison.ajouter();
+        //this.constructor.ajouter();
 	}
 	perimetre() {
 		return (this.longueur + this.largeur) * 2;
@@ -24,15 +27,20 @@ class Maison {
 	surface() {
 		return this.longueur * this.largeur * this.nbrEtage;
 	}
+    static ajouter(){
+        Maison.nbrDeMaison++ 
+    }
 }
 //instancier un objet Maison
 const villa = new Maison("villa", 10, 20);
 console.log(`Le périmètre de ${villa.nom} est égal à : ${villa.perimetre()} m`);
 console.log(`La surface de ${villa.nom} est égale à : ${villa.surface()} m²`);
-const immeuble = new Maison("immeuble", 30, 40);
+//const immeuble = new Maison("immeuble", 30, 40);
 console.log(
 	`Le périmètre de ${immeuble.nom} est égal à : ${immeuble.perimetre()} m`
 );
 console.log(
 	`La surface de ${immeuble.nom} est égale à : ${immeuble.surface()} m²`
 );
+const magasin = new Maison("magasin", 40, 20);
+console.log(`On à instancié : ${Maison.nbrDeMaison} ${Maison.nbrDeMaison > 1?'Maisons':'Maison'} `);
