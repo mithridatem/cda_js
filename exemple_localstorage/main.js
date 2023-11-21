4910d26874ee6a4ad43e246f3e1a945219c27ff8
mithridatem <mathieumithridate@adrar-formation.com>
2023-11-21 16:12:44 +0100

    exemple localstorage

---
+++ exemple_localstorage/main.js
@@ -0,0 +1,8 @@
+//ajouter une clé dans le localstorage (clé valeur)
+localStorage.setItem('nom', 'Mathieu');
+//récupérer une clé dans le localstorage (nom clé)
+const recup = localStorage.getItem('nom2');
+//supprimer une clé par son nom clé
+localStorage.removeItem('nom');
+//supprimer toutes les clés
+localStorage.clear();
