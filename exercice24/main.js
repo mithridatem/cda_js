f0e67857b9c954ca5a1839c0865ad367806a2f4a
mithridatem <mathieumithridate@adrar-formation.com>
2023-11-21 16:32:19 +0100

    correction exercice 24 base

---
+++ exercice24/main.js
@@ -0,0 +1,62 @@
+/* En vous inspirant de l'exemple ci-dessus :
+Partie HTML :
+-un titre h1 = liste des tâches,
+-un input de type text (id= task),
+-un bouton (id = add, onclick = addTask()) texte Ajouter,
+-un bouton (id = delAllTask, onclick = delAllTask()) texte Tout Supprimer,
+-un bouton (id = reload, onclick = reload()) texte Recharger la page,
+-une div (id = tasks)
+Partie JS :
+-Créer une fonction addtask qui va ajouter à chaque clic sur le bouton 
+une nouvelle tache à la div (id = task),:
+-Récupérer la div (tasks)
+Récupérer la valeur de l'input (id task),
+-Créer un paragraphe,
+Ajouter la valeur de l'input (id task)  au paragraphe,
+Ajouter le paragraphe à la div (id = tasks)
+-Créer une fonction delAllTask qui va à chaque clic sur le bouton supprimer tous 
+les enfants (child) contenu dans la div (tasks),
+-Créer une fonction reload qui va à chaque clic sur le bouton recharger la page.
+
+Bonus :
+-Dans la fonction addTask (remplacer le code précédent) :
+A chaque clic sur la fonction : 
+      -ajouter une div (class = container),
+          -> à l'intérieur de la div pour aller ajouter un paragraphe,
+          -> récupérer le contenu de l'input texte (task) et passer la valeur au paragraphe,
+          -> ajouter un bouton (id = delete, onclick = deleteTask(this),
+          -> ajouter un bouton (id = update, onclick = updateTask(this),
+          ->Ajouter une fonction deleteTask qui va :
+             Supprimer la tache sélectionnée,
+          -Ajouter une fonction updateTask qui va :
+            Mettre à jour le nom de la tache depuis l'input du formulaire (id = task)
+NB : on à besoin d'utiliser la création d'élément (createElement() et appendChild()) */
+
+//récupération de la div (id="tasks")
+const tasks = document.querySelector('#tasks');
+//function addTask
+function addTask(){
+    //récupération de la valeur saisie dans l'input task
+    const task = document.querySelector('#task').value;
+    if(task!=""){
+        //créer un paragraphe
+        const paragraphe = document.createElement('p');
+        //ajouter la valeur de l'input au nouveau paragraphe
+        paragraphe.textContent = task;
+        //ajouter le paragraphe à la div
+        tasks.appendChild(paragraphe);
+    }
+}
+function delAllTask(){
+    //boucle pour parcourir le premier enfant
+    while(tasks.firstChild){
+        //supprimer le premier enfant
+        tasks.firstChild.remove();
+    }
+}
+function reload(){
+    //alternative
+    //window.location.reload();
+    //version raccourci
+    location.reload();
+}
